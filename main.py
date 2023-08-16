32195e34e5aa2aa7ac3cad11b8a384b4af3062a3
hadirezay <mo.hadirezay@gmail.com>
2023-08-16 21:33:24 +1000

    Update main.py
    Version 1.5
    Bugs still to fix, API call request per minute issue

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World")
+import os
+import openai
+import time
+
+#openai.api_key = "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"
+
+#Below is a basic code to open the API key file which will later be modify to store key
+#openai.api_key = open("keyfile.txt", "r").read().strip('\n')
+
+
+home_menu = (1, 2)
+API_Key = input("Enter your OpenAI key: ")
+#print(API_Key)
+
+print("Choose module")
+print("1 = Question Dataset")
+print("2 = Chatbot")
+user_choice = input()
+if user_choice == 1:
+    openai.api_key = "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"
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "You are a helpful assistant."}]
+    )
+    response = completion.choices[0].message.content
+    print(response)
