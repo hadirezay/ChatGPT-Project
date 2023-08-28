8436f94685a0254c34f12b48a8dfbdc7d6002646
Noah Raines <103612712@student.swin.edu.au>
2023-08-28 16:29:11 +1000

    Started Overhauling GUI
    - Created Main window with help of example given to me by Matthew
    - Converted TopLevel Windows into frames, allowing for them to be navigated via the navigation bar
    - Commented/deleted depreciated code of the previous UI system

--- GUI/chatbot_window.py
+++ GUI/chatbot_window.py
@@ -6,8 +6,77 @@ from functools import partial
 # SQLAlchemy for inserting data into the database
 # Import for openAI API, in order to load and start the chat, as well as to set appropriate parameters
 
+class chatbot_window(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.title = ctk.CTkLabel(self, text="ChatBot Module", width=120,height=25)
+        self.title.grid(row=0, column=0, padx=20, pady=10)
 
-class chatbot_window(ctk.CTkToplevel):
+        self.model_btn = ctk.CTkButton(self, text="Select Model", command=self.model_window)
+        self.model_btn.grid(row=1, column=0, padx=20, pady=10)
+
+        self.param_btn = ctk.CTkButton(self, text="Modify Parameters")
+        self.param_btn.grid(row=2, column=0, padx=20, pady=10)
+
+        self.execute_btn = ctk.CTkButton(self, text="Execute Chat")
+        self.execute_btn.grid(row=3, column=0, padx=20, pady=10)
+
+        self.model_frame = model_frame(self)
+
+    def model_window(self):
+        self.grid_forget()
+        self.model_frame.grid(row=0, column=0, sticky="nsew")
+        
+
+class model_frame(ctk.CTkFrame):
+     def __init__(self, master):
+        super().__init__(master)
+        self.title = ctk.CTkLabel(self, text="Select Version", width=120,height=25)
+        self.title.grid(row=0, column=0, padx=20, pady=10)
+
+        self.model_btn = ctk.CTkButton(self, text="3.5")
+        self.model_btn.grid(row=1, column=0, padx=20, pady=10)
+
+        self.param_btn = ctk.CTkButton(self, text="4.0")
+        self.param_btn.grid(row=2, column=0, padx=20, pady=10)
+
+
+class param_frame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.title = ctk.CTkLabel(self, text="Modify Paramaters", width=120,height=25)
+        self.title.grid(row=0, column=0, padx=20, pady=10)
+
+        self.model_btn = ctk.CTkButton(self, text="3.5")
+        self.model_btn.grid(row=1, column=0, padx=20, pady=10)
+
+        self.param_btn = ctk.CTkButton(self, text="4.0")
+        self.param_btn.grid(row=2, column=0, padx=20, pady=10)
+
+class chatting_frame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.chat_box = ctk.CTkTextbox(self, height=400, width=500)
+        self.chat_box.grid(row=0, column=0, padx=10, pady=10)
+
+        self.chat_entry = ctk.CTkEntry(
+            self,
+            placeholder_text="Enter a chat here"
+        ).grid(row=1, column=0, padx=10, pady=10)
+
+        self.enter_btn = ctk.CTkButton(
+            self,
+            text="Enter Chat",
+            command=self.enter_chat
+        ).grid(row=1, column=0, padx=20, pady=10)
+
+    def enter_chat(self):
+        print('Chat Entered')
+        
+  
+
+""" class chatbot_window(ctk.CTkToplevel):
     def __init__(self):
         super().__init__()
 
@@ -100,4 +169,4 @@ class chatbot_window(ctk.CTkToplevel):
 
     def clear_window(self):
         for widget in self.winfo_children():
-            widget.destroy()
+            widget.destroy() """
--- GUI/main_window.py
+++ GUI/main_window.py
@@ -5,44 +5,66 @@ from GUI.history_window import history_window
 
 # Window is built into class, initialised with ctk.CTk variable
 class main_window(ctk.CTk):
+    width = 900
+    height = 600
     def __init__(self):
         super().__init__()
-        self.main_frame = ctk.CTkFrame(self, corner_radius=0)
-        self.main_frame.grid_columnconfigure(0, weight=1)
-        self.main_label = ctk.CTkLabel(
-            self.main_frame,
-            text="CustomTkinter\nMain Page",
-            font=ctk.CTkFont(size=20, weight="bold"))
-        
-        self.main_label.grid(row=0, column=0, padx=30, pady=(30, 15))
-        self.back_button = ctk.CTkButton(self.main_frame, text="Back", command=self.back_event, width=200)
-        self.back_button.grid(row=1, column=0, padx=30, pady=(15, 15))
+        self.title("ChatGPT Assistant")
+        self.geometry(f"{self.width}x{self.height}")
 
-        """ self.title("Chat GPT Assistant")
-        self.geometry("800x600")
-        greeting_lbl = ctk.CTkLabel(
-            self,
-            text="Chat GPT"
-        ).pack()
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
-        chatbot_btn = ctk.CTkButton(
-            self,
-            text="ChatBot",
-            command=self.chatbot_window
-        ).pack()
+        #Navigation frame
+        self.navigation_frame = ctk.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-        dataset_btn = ctk.CTkButton(
-            self, 
-            text="Dataset",
-            command=self.select_file
-        ).pack()
-        
-        history_btn = ctk.CTkButton(
-            self,
-            text="History",
-            command=self.question_history
-        ).pack()
- """
+        self.navigation_frame_label = ctk.CTkLabel(self.navigation_frame, text="  ChatGPT Assistant",
+                                                             compound="left", font=ctk.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        self.home_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home",
+                                                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                 anchor="w", command=self.home_window)
+        self.home_button.grid(row=1, column=0, sticky="ew")
+
+        self.chatbot_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="ChatBot",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                      anchor="w", command=self.chatbot_window)
+        self.chatbot_button.grid(row=2, column=0, sticky="ew")
+
+        self.history_button = ctk.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="History",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                      anchor="w", command=self.question_history)
+        self.history_button.grid(row=3, column=0, sticky="ew")
+
+        self.appearance_mode_menu = ctk.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],)
+        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+
+        #Home frame
+        self.home_frame = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame.grid(row=0, column=1, sticky="nsew")
+        self.home_frame.grid_columnconfigure(0, weight=1)
+
+        self.welcome_label = ctk.CTkLabel(self.home_frame, text="Welcome to the ChatGPT Assistant", width=120,height=25)
+        self.welcome_label.grid(row=0, column=0, padx=20, pady=10)
+
+        self.home_frame_button_1 = ctk.CTkButton(self.home_frame, text="")
+        self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.home_frame_button_2 = ctk.CTkButton(self.home_frame, text="CTkButton", compound="right")
+        self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.home_frame_button_3 = ctk.CTkButton(self.home_frame, text="CTkButton", compound="top")
+        self.home_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
+        self.home_frame_button_4 = ctk.CTkButton(self.home_frame, text="CTkButton",compound="bottom", anchor="w")
+        self.home_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
+
+        self.chatbot_frame = chatbot_window(self)
+        self.chatbot_frame.grid_columnconfigure(0, weight=1)
+
+        self.history_frame = history_window(self)
+        self.history_frame.grid_columnconfigure(0, weight=1)
+
     #Function for selecting file
     #Only takes in .csv and .json files as an argument
     def select_file(self):
@@ -73,9 +95,26 @@ class main_window(ctk.CTk):
         #TODO Loads response window upon successful, taking the file as an argument
 
     #Loads the history window of all of the recent questions assessed
-    def question_history(self):
-        history_window()
+    def frame_select(self, name):
+        if name == "Home":
+            self.home_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.home_frame.grid_remove()
+        if name == "ChatBot":
+            self.chatbot_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.chatbot_frame.grid_remove()
+        if name == "History":
+            self.history_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.history_frame.grid_remove()
+
+    def home_window(self):
+        self.frame_select("Home")
 
     # Loads the chatbot window via use of a toplevel window
     def chatbot_window(self):
-        chatbot_window()
+        self.frame_select("ChatBot")
+
+    def question_history(self):
+        self.frame_select("History")
