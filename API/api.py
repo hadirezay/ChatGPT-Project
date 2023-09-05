55bb599fc7340dfc89fece3a85b91f6841f22535
hadirezay <mo.hadirezay@gmail.com>
2023-09-05 20:49:59 +1000

    moving

---
+++ API/api.py
@@ -0,0 +1,64 @@
+import os
+import openai
+import time
+import csv
+
+
+# Below is a basic code to open the API key file which will later be integrated with UI
+# openai.api_key = open("keyfile.txt", "r").read().strip('\n')
+
+# Below function is for dataset module
+def call_dataset(key):
+    file = open('newtext.txt', 'r')
+    questions = file.readlines()
+    i = 1
+    for line in questions:
+        openai.api_key = key
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=line,
+            max_tokens=100,
+            temperature=0
+        )
+        print("Question", i, ": ", line)
+        i += 1
+        print("Answer: ", response['choices'][0]['text'])
+        print("\n")
+
+#Below function is for Chatbot module
+def call_chat(key):
+    user_model_choice = ""
+    print("Select Language model")
+    print("Press 1 for gpt-4")
+    print("Press 2 for gpt-3.5-turbo")
+    print("Or Enter a language model, follow OpenAI's Model endpoint compatibility: https://platform.openai.com/docs/models/model-endpoint-compatibility")
+    option = input("Language model choice: ")
+    if option == "1":
+        user_model_choice = "gpt-4"
+    elif option == "2":
+        user_model_choice = "gpt-3.5-turbo"
+    while True:
+        openai.api_key = key
+        # user_input = input("User: ")
+        completion = openai.ChatCompletion.create(
+            model=user_model_choice,
+            messages=[{"role": "user", "content": input("User: ")}]
+        )
+        response = completion.choices[0].message.content
+        print("ChatGPT:", response)
+
+
+
+# Main
+print("Group 20 - OpenAI chat & completion Home Page")
+API_Key = input("Insert your API KEY: ")
+
+print("Choose module")
+print("Type 1 for Chatbot")
+print("Type 2 for Dataset insertion")
+user_choice = input()
+
+if user_choice == "1":
+    call_chat(API_Key)
+elif user_choice == "2":
+    call_dataset(API_Key)
